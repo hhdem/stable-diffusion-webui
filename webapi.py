1f82245ebdf0e90af4f7c0c3e0f22cd5dfdc330b
luke <liubingchina@gmail.com>
2023-05-03 11:37:07 +0300

    default sampler_index to 'DPM++ SDE Karras'

--- webapi.py
+++ webapi.py
@@ -20,11 +20,12 @@ def generateImage(prompt:str = "a cat", width:int = 512, height:int = 512, model
     if model != "":
         checkpoint = modules.sd_models.select_checkpoint(model)
         modules.sd_models.load_model(checkpoint)
+
     id_task = "my-task"
     negative_prompt = ""
     prompt_styles = []
     steps = 11
-    sampler_index = 0
+    sampler_index = 'DPM++ SDE Karras'
     restore_faces = False
     tiling = False
     n_iter = 1
